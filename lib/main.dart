519cc932e111f47e5e1c5f8f9eb0ec3c912dd734
aashahin <almhtrfahmed123@gmail.com>
2023-05-23 21:59:19 +0300

    Added Counter

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:learn_flutter/messenger_screen.dart';
-import 'package:learn_flutter/users_screen.dart';
+import 'package:learn_flutter/counter_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +12,7 @@ class MyApp extends StatelessWidget{
   Widget build(BuildContext context){
     return const MaterialApp(
       title: "Learn Flutter",
-      home: MessengerScreen(),
+      home: CounterScreen(),
       debugShowCheckedModeBanner: false,
     );
   }
